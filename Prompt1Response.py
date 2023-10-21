b23ff8aecf35fd034e82c1ac6357837776e9defc
Jbenno-McBoyo <146880037+Jbenno-McBoyo@users.noreply.github.com>
2023-10-21 16:29:40 -0700

    Prompt 1 for CJ1

---
+++ Prompt1Response.py
@@ -0,0 +1,5 @@
+user_name = input('What is your full name?')
+user_prons = input('What are your pronouns?')
+user_bio = input('Tell me about your favorite movie and food?')
+
+print(f'{user_name} ({user_prons}), wants to tell you:\n{user_bio}')
